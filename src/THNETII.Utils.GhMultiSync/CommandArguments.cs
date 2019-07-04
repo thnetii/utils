5c2366fc41bd100ed7d6bae5dff498667b0b1d13
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-07-04 13:29:18 +0200

    Add File argument

--- src/THNETII.Utils.GhMultiSync/CommandArguments.cs
+++ src/THNETII.Utils.GhMultiSync/CommandArguments.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.CommandLine;
-using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace THNETII.Utils.GhMultiSync
 {
@@ -16,8 +17,38 @@ namespace THNETII.Utils.GhMultiSync
 
             Token = parseResult.FindResultFor(definition.TokenOption)?
                 .GetValueOrDefault<string>();
+
+            FileReaders = parseResult.FindResultFor(definition.FileArgument)?
+                .GetValueOrDefault<string[]>()?
+                .Distinct(DashStringDistincEqualityComparer.Instance)
+                .Select<string, (string, Func<TextReader>)>(p =>
+                {
+                    if (p == "-")
+                        return ("<STDIN>", GetConsoleInTextReader);
+                    else
+                        return (p, () => File.OpenText(p));
+                })
+                .ToList();
+            if (FileReaders.Count == 0)
+                FileReaders.Add(("-", GetConsoleInTextReader));
+
+            TextReader GetConsoleInTextReader() => Console.In;
         }
 
+        public CommandDefinition Definition { get; }
+        public ParseResult ParseResult { get; }
         public string Token { get; set; }
+        public List<(string path, Func<TextReader> factory)> FileReaders { get; }
+
+        private class DashStringDistincEqualityComparer : IEqualityComparer<string>
+        {
+            public static DashStringDistincEqualityComparer Instance { get; } =
+                new DashStringDistincEqualityComparer();
+
+            private DashStringDistincEqualityComparer() { }
+
+            public bool Equals(string x, string y) => (x is "-" && y is "-");
+            public int GetHashCode(string obj) => obj?.GetHashCode(StringComparison.Ordinal) ?? 0;
+        }
     }
 }
--- src/THNETII.Utils.GhMultiSync/CommandDefinition.cs
+++ src/THNETII.Utils.GhMultiSync/CommandDefinition.cs
@@ -1,5 +1,9 @@
-﻿using System.CommandLine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace THNETII.Utils.GhMultiSync
@@ -10,14 +14,19 @@ namespace THNETII.Utils.GhMultiSync
         {
             var rootCommand = CreateRootCommand(commandHandler);
 
+            var fileArgument = CreateFileArgument();
+            rootCommand.AddArgument(fileArgument);
+
             var tokenOption = CreateTokenOption();
             rootCommand.AddOption(tokenOption);
 
             RootCommand = rootCommand;
+            FileArgument = fileArgument;
             TokenOption = tokenOption;
         }
 
         public RootCommand RootCommand { get; }
+        public Argument<string[]> FileArgument { get; }
         public Option TokenOption { get; }
 
         private static string GetDescription() => typeof(Program).Assembly
@@ -42,6 +51,44 @@ namespace THNETII.Utils.GhMultiSync
             opt.AddAlias("-t");
 
             return opt;
+        }
+
+        private static Argument<string[]> CreateFileArgument()
+        {
+            const string file = nameof(file);
+            var arg = new Argument<string[]>(file.ToUpperInvariant())
+            {
+                Description = "Multi-sync specs to execute. Omit or '-': <STDIN>",
+                Arity = ArgumentArity.ZeroOrMore
+            };
+            arg.AddValidator(symbol => symbol.Tokens.Select(t => t.Value)
+                .Where(p => p != "-" && !File.Exists(p))
+                .Select(ValidationMessages.Instance.FileDoesNotExist)
+                .FirstOrDefault()
+                );
+            //arg.AddSuggestionSource(SuggestFileSystemEntries);
+
+            return arg;
+        }
+
+        private static IEnumerable<string> SuggestFileSystemEntries(string pathToMatch)
+        {
+            if (string.IsNullOrEmpty(pathToMatch))
+                return SuggestFileSystemEntries(".");
+
+            string directoryName, searchPattern;
+            if (Directory.Exists(pathToMatch))
+                (directoryName, searchPattern) = (pathToMatch, "*");
+            else
+            {
+                directoryName = Path.GetDirectoryName(pathToMatch);
+                if (string.IsNullOrEmpty(directoryName))
+                    directoryName = ".";
+                searchPattern = Path.GetFileName(pathToMatch) + "*";
+            }
+
+            return Directory.EnumerateFileSystemEntries(directoryName, searchPattern, SearchOption.TopDirectoryOnly)
+                .Select(path => Directory.Exists(path) ? path + Path.DirectorySeparatorChar : path);
         }
     }
 }
